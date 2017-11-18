bd80f08518f41e9c779e1b48f8db4341631f67e0
kalimess16 <33270591+kalimess16@users.noreply.github.com>
2017-11-18 19:23:23 +0700

    check

--- ConnectServer.py
+++ ConnectServer.py
@@ -4,12 +4,12 @@ from  AnalysicFirebase import  __requests__,__results__
 from clarifai.rest import ClarifaiApp
 from clarifai.rest import Image as ClImage
 from pprint import pprint
-# import  datetime
-# tick = datetime.datetime.now()
-# str = tick.strftime("%Y-%m-%d %H:%M:%S")
+import  datetime
+tick = datetime.datetime.now()
+str = tick.strftime("%Y-%m-%d %H:%M:%S")
 # #print(str)
 t='2017-11-18 16:18:09'
-rs=__results__('2017-11-18 16:18:09')
+rs=__results__(str)
 print(rs)
 
 app = ClarifaiApp(api_key="d23a9b2861af443ba96d80763d365ad9")
@@ -18,3 +18,6 @@ response = model.predict_by_url(rs)
 predict_name= response['outputs'][0]['data']['concepts'][0]["id"]
 pri_percent = response['outputs'][0]['data']['concepts'][0]["value"]
 re = __requests__('2017-11-18 16:18:09',predict_name,pri_percent)
+file_name = 'output'+ '.json'
+with codecs.open(file_name,'w','utf-8') as ww:
+  json.dump(response,ww,ensure_ascii=False,indent=2)
