79b7d73b6ccf17a55e65dbb3c0e06570c9fd26b3
kalimess16 <33270591+kalimess16@users.noreply.github.com>
2017-11-18 20:24:07 +0700

    test

--- ConnectServer.py
+++ ConnectServer.py
@@ -7,17 +7,16 @@ from pprint import pprint
 import  datetime
 tick = datetime.datetime.now()
 str = tick.strftime("%Y-%m-%d %H:%M:%S")
-# #print(str)
-t='2017-11-18 16:18:09'
+print(str)
+t='2017-11-18 20:09:25'
 rs=__results__(str)
 print(rs)
-
 app = ClarifaiApp(api_key="d23a9b2861af443ba96d80763d365ad9")
 model = app.models.get('AIBoy')
 response = model.predict_by_url(rs)
 predict_name= response['outputs'][0]['data']['concepts'][0]["id"]
 pri_percent = response['outputs'][0]['data']['concepts'][0]["value"]
-re = __requests__('2017-11-18 16:18:09',predict_name,pri_percent)
+re = __requests__(str,predict_name,pri_percent)
 file_name = 'output'+ '.json'
 with codecs.open(file_name,'w','utf-8') as ww:
   json.dump(response,ww,ensure_ascii=False,indent=2)
