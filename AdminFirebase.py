0ca4872a716fbafa519d90487b62006ec483b081
kalimess16 <33270591+kalimess16@users.noreply.github.com>
2017-11-19 07:41:29 +0700

    a

--- AdminFirebase.py
+++ AdminFirebase.py
@@ -1,5 +1,34 @@
 import firebase_admin
 from firebase_admin import credentials
+from clarifai.rest import ClarifaiApp
+from clarifai import  rest
+from clarifai.rest import Image as ClImage
+from firebase_admin import db
+# Fetch the service account key JSON file contents
+import firebase_admin
+from firebase_admin import credentials
+cred = credentials.Certificate('flycam-184516-firebase-adminsdk-fps0w-307954056b.json')
+# default_app = firebase_admin.initialize_app(cred)
+#
+# print(default_app.name)
 
-cred = credentials.Certificate('path/to/serviceAccountKey.json')
-default_app = firebase_admin.initialize_app(cred)
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://flycam-184516.firebaseio.com/'
+})
+
+# As an admin, the app has access to read and write all data, regradless of Security Rules
+
+ref = db.reference('Image')
+result = ref.order_by_key().limit_to_last(1).get()
+dateTimeKey = list(result.keys())[0]
+imageLink = result[dateTimeKey]['link']
+print(imageLink)
+app = ClarifaiApp(api_key="d23a9b2861af443ba96d80763d365ad9")
+model = app.models.get('AIBoy')
+response = model.predict_by_url(imageLink)
+predict_name= response['outputs'][0]['data']['concepts'][0]["id"]
+pri_percent = response['outputs'][0]['data']['concepts'][0]["value"]
+refPredict = db.reference('Predict')
+
+refPredict.push(ref.order_by_child('date').limit_to_last(1).get())
--- ConnectServer.py
+++ ConnectServer.py
@@ -2,21 +2,43 @@ import json
 import codecs
 from  AnalysicFirebase import  __requests__,__results__
 from clarifai.rest import ClarifaiApp
+from clarifai import  rest
 from clarifai.rest import Image as ClImage
 from pprint import pprint
-import  datetime
-tick = datetime.datetime.now()
-str = tick.strftime("%Y-%m-%d %H:%M:%S")
-print(str)
-t='2017-11-18 20:09:25'
-rs=__results__(str)
-print(rs)
-app = ClarifaiApp(api_key="d23a9b2861af443ba96d80763d365ad9")
+import firebase_admin
+from firebase_admin import credentials
+
+from firebase_admin import db
+# Fetch the service account key JSON file contents
+import firebase_admin
+from firebase_admin import credentials
+
+
+cred = credentials.Certificate('flycam-184516-firebase-adminsdk-fps0w-307954056b.json')
+# default_app = firebase_admin.initialize_app(cred)
+#
+# print(default_app.name)
+
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://flycam-184516.firebaseio.com/'
+})
+
+ref = db.reference('Image')
+result = ref.order_by_key().limit_to_last(1).get()
+dateTimeKey = list(result.keys())[0]
+imageLink = result[dateTimeKey]['link']
+print(imageLink)
+app = ClarifaiApp(api_key='d23a9b2861af443ba96d80763d365ad9')
 model = app.models.get('AIBoy')
-response = model.predict_by_url(rs)
+model.get_version('d1f91e14ce6145bfb01ce2e9461e0192')
+response = model.predict_by_url(imageLink)
 predict_name= response['outputs'][0]['data']['concepts'][0]["id"]
 pri_percent = response['outputs'][0]['data']['concepts'][0]["value"]
-re = __requests__(str,predict_name,pri_percent)
-file_name = 'output'+ '.json'
-with codecs.open(file_name,'w','utf-8') as ww:
-  json.dump(response,ww,ensure_ascii=False,indent=2)
+print(predict_name,"--",pri_percent)
+refPredict = db.reference()
+request = refPredict.child("Predict").set({
+    'dudoan':predict_name,
+    'xs':pri_percent
+})
+
